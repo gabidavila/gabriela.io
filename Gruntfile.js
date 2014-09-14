c2bd25dc43574a3c721cfd615575cde641943c3b
Gabriela D'Avila <gabidavila@gmail.com>
2014-09-14 13:38:15 -0300

    new template skeleton

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,8 @@ module.exports = function(grunt) {
                 vendor: './source/assets/vendor/'
             },
             template : {
-                freelancer: './source/assets/vendor/freelancer/'
+                freelancer: './source/assets/vendor/freelancer/',
+                personal: './source/assets/vendor/freelancer/'
             },
             // Production where Grunt output the files
             css: './source/css/',
@@ -26,13 +27,13 @@ module.exports = function(grunt) {
                 options: {
                     separator: ';',
                 },
-                js_header: {
+                freelancer_js_header: {
                     src: [
                         '<%= paths.assets.js %>freelancer/custom.js'
                     ],
                     dest: '<%= paths.js %>freelancer/scripts_header.js',
                 },
-                js_footer: {
+                freelancer_js_footer: {
                     src: [
                         '<%= paths.assets.vendor %>jquery/dist/jquery.js',
                         '<%= paths.assets.vendor %>bootstrap/dist/js/bootstrap.js',
@@ -44,7 +45,26 @@ module.exports = function(grunt) {
                         '<%= paths.template.freelancer %>js/freelancer.js'
                     ],
                     dest: '<%= paths.js %>freelancer/scripts_footer.js',
-                }
+                },
+            personal_js_header: {
+                src: [
+                    '<%= paths.assets.js %>personal/custom.js'
+                ],
+                dest: '<%= paths.js %>personal/scripts_header.js',
+            },
+            personal_js_footer: {
+                src: [
+                    '<%= paths.assets.vendor %>jquery/dist/jquery.js',
+                    '<%= paths.assets.vendor %>bootstrap/dist/js/bootstrap.js',
+                    '<%= paths.assets.vendor %>jquery.easing.1.3/index.js',
+                    '<%= paths.template.personal %>js/classie.js',
+                    '<%= paths.template.personal %>js/cbpAnimatedHeader.js',
+                    '<%= paths.template.personal %>js/jqBootstrapValidation.js',
+                    '<%= paths.template.personal %>js/contact_me.js',
+                    '<%= paths.template.personal %>js/freelancer.js'
+                ],
+                dest: '<%= paths.js %>personal/scripts_footer.js',
+            }
             //}
         },
         less: {
@@ -57,6 +77,17 @@ module.exports = function(grunt) {
                     "<%= paths.css %>freelancer/freelancer.min.css":"<%= paths.assets.css %>freelancer/freelancer.less",
                     //compiling styles.less into styles.min.css
                     "<%= paths.css %>freelancer/styles.min.css":"<%= paths.assets.css %>freelancer/styles.less"
+                }
+            },
+            personal: {
+                options: {
+                    compress: true,  //minifying the result
+                },
+                files: {
+                    //compiling styles.less into styles.min.css
+                    "<%= paths.css %>personal/freelancer.min.css":"<%= paths.assets.css %>personal/freelancer.less",
+                    //compiling styles.less into styles.min.css
+                    "<%= paths.css %>personal/styles.min.css":"<%= paths.assets.css %>personal/styles.less"
                 }
             }
         },
